549b31c3037e04645a6cb3de76e4a7a6687dcf8f
Abhilash Kumbhum <kumbhu@amazon.com>
2019-11-21 20:54:32 +0530

    Added comments and explanation

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,14 +2,13 @@ package controllers
 
 import javax.inject._
 import models.Task
-import play.api._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
+ * application's home page. Each method in this controller maps to a route. See conf/routes file
  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents)
@@ -21,7 +20,7 @@ class HomeController @Inject()(cc: ControllerComponents)
    *
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
-   * a path of `/`.
+   * a path of `/`. Here we are simply sending a redirect response 303 to tell the client to redirect to /tasks
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Redirect(routes.HomeController.tasks())
@@ -39,24 +38,24 @@ class HomeController @Inject()(cc: ControllerComponents)
     taskForm.bindFromRequest.fold(
       errors => BadRequest(views.html.tasks(Task.all(), errors)),
       label => {
-        Task.create(label)
-        Redirect(routes.HomeController.tasks)
+        /**
+          * TODO: [TASK-2]
+          * 1.Use the model class to call the create method
+          * 2. Remove the Ok response below and send the redirect(refer to index method) response to go to /tasks page
+          */
+        Ok
       }
     )
   }
 
-  def deleteTask(id: Long) = Action {
-    Task.delete(id)
-    Redirect(routes.HomeController.tasks)
-  }
+  /**
+    * TODO: [TASK-5] You need to implement the method to do the following
+    * 1. Use the form binding to get the form data
+    * 2. In the success function, call the update method of the model class to update the given task with id
+    * 3. Redirect the user to /tasks
+    * @param id
+    * @return
+    */
+  def updateTask(id: Long) = TODO
 
-  def updateTask(id: Long) = Action { implicit request =>
-    taskForm.bindFromRequest.fold(
-      errors => BadRequest(views.html.tasks(Task.all(), errors)),
-      label => {
-        Task.update(id, label)
-        Redirect(routes.HomeController.tasks)
-      }
-    )
-  }
 }
--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,17 +1,48 @@
 package models
 
+/**
+  * Simple POJO to model immutable data. https://docs.scala-lang.org/tour/case-classes.html
+  * @param id
+  * @param label
+  */
 case class Task(id: Long, label: String)
 
+/**
+  * Companion singleton object that holds static variables and methods
+  * https://docs.scala-lang.org/tour/singleton-objects.html
+  */
 object Task {
 
+  /**
+    * Static collection to act as in-memory DB. Feel free to override this to use any data structure you like
+    */
   val tasks = collection.mutable.Map[Long, String]()
 
-  def all(): List[Task] = Nil
+  /**
+    * Method to return the list of tasks that the user has added till now
+    * @return
+    */
 
-  def create(label: String) {}
+  def all(): List[Task] = {
+    Task(1, "Hardcoded task 1") :: Task(2, "Hardcoded task 2") :: Nil
+    //TODO [TASK-1] Please complete business logic to return all the task from 'tasks' and remove the hard coding.
+  }
 
-  def delete(id: Long) {}
+  /**
+    * Method to create a task and add it to the existing tasks
+    * @param label
+    */
+  def create(label: String): Unit = {
+    //TODO [TASK-3] Please complete business to create a new task and add it to the existing
+  }
 
-  def update(id: Long, label: String) {}
+  /**
+    * Method to update a label of task with given id
+    * @param id
+    * @param label
+    */
+  def update(id: Long, label: String): Unit = {
+    //TODO [TASK-6] Please complete business to update a given task
+  }
 
 }
